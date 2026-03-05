cfd00aca39a8eadb479c6db03f7256fbff21b69e
VinceDriesen <vince.driesen@student.uhasselt.be>
2026-03-05 09:49:12 +0100

    ff client

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from trello import TrelloClient
+
+api="piemol"
+secret="secret"
+
+client = TrelloClient(
+    api_key=api,
+    api_secret=secret,
+    token='your-oauth-token-key',
+    token_secret='your-oauth-token-secret'
+)
+
 def main():
     print("Kill me right now")
 
