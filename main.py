3fbdd6f1ea9f685134557bdb420b025bce6810dc
Daan Hollands <daan.hollands@student.uhasselt.be>
2026-03-05 09:52:09 +0100

    Hier keys man

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from trello import TrelloClient
 
-api="piemol"
-secret="secret"
+api = "31ec916f741962caeb3b4d2ca1fd43b7"
+secret = "adf924cd87998ca0832d5ad7adf015d7ac89765efd5c9390900f94faad14695b"
 
 client = TrelloClient(
     api_key=api,
     api_secret=secret,
 )
 
+
 def main():
     boards = client.list_boards()
     print(boards[-1].name)
-    print("Kill me right now")
 
 
 if __name__ == "__main__":
