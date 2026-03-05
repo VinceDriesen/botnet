6dd230347c5f4ee7c4881b551319108d70041a04
VinceDriesen <vince.driesen@student.uhasselt.be>
2026-03-05 10:21:53 +0100

    Ff de ids allemaal toegevoegd

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from trello import TrelloClient
 
 api = "31ec916f741962caeb3b4d2ca1fd43b7"
 token = "ATTA499aed423e1f8c8d9e6b7342e9fdc93a88053e51526037e58616fb50867236033EB1F029"
+board_id = "8Gjc8fX8"
+status_list_id = "69a946c9af7564bacbed187d"
+command_list_id = "69a946cd58f076e0489335cb"
 
 client = TrelloClient(
     api_key=api,
@@ -10,8 +13,8 @@ client = TrelloClient(
 
 
 def main():
-    boards = client.list_boards()
-    print(boards[-1].name)
+    board = client.get_board(board_id=board_id)
+    print(board.get_list(status_list_id))
 
 
 if __name__ == "__main__":
